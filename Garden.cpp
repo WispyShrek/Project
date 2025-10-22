d9d983ccde6ff33bf5744ed5acbf5676c3d27c4f
CheriseHeyl <cheriseheyl@gmail.com>
2025-10-22 14:49:22 +0200

    Implemented attach, detach and notify functions for the Garden class with Doxygen comments for documentation.

--- Garden.cpp
+++ Garden.cpp
@@ -1,50 +1,30 @@
 #include "Garden.h"
+#include <algorithm>
 
+template <typename T> void Garden<T>::addItem(Garden *item) {}
 
+template <typename T> Iterator *Garden<T>::CreateIterator() { return nullptr; }
 
-template <typename T>
-void Garden<T>::addItem(Garden *item){
-}
-
-template <typename T>
-Iterator *Garden<T>::CreateIterator()
-{
-    return nullptr;
-}
-
-//template <typename T>
-//Garden *Garden<T>::getCurrItem()
+// template <typename T>
+// Garden *Garden<T>::getCurrItem()
 //{
-//    return nullptr;
-//}
+//     return nullptr;
+// }
 
-template <typename T>
-bool Garden<T>::isEmpty()
-{
-    return false;
+template <typename T> bool Garden<T>::isEmpty() { return false; }
+
+template <typename T> void Garden<T>::removeItem(T item) {}
+
+void applyCare() {}
+
+void Garden::attach(PlantCaretaker *staff) { this->staffList.push_back(staff); }
+
+void Garden::detach(PlantCaretaker *staff) {
+  this->staffList.erase(std::find(staffList.begin(), staffList.end(), staff));
 }
 
-template <typename T>
-void Garden<T>::removeItem(T item)
-{
-}
-
-template <typename T>
-void Garden<T>::TemplateMethod()
-{
-}
-
-template <typename T>
-void Garden<T>::attach(Staff *staff)
-{
-}
-
-template <typename T>
-void Garden<T>::detach(Staff *staff)
-{
-}
-
-template <typename T>
-void Garden<T>::notify()
-{
+void Garden::notify() {
+  for (auto staff : staffList) {
+    staff->update(this);
+  }
 }
